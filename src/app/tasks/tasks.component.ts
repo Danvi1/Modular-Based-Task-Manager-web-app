f223f2d89d5d2e6e702f2277a05662abb16f948f
dhnas <danydhanvith@gmail.com>
2026-02-20 15:28:43 +0530

    using task component to display user tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+
+import { DummyTasks } from './dummy-tasks'
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
     @Input() name?: string;
+    @Input() id?: string;
+    
+    tasks = DummyTasks;
 
+    get userTasks() {
+        return this.tasks.filter((task) => task.userId === this.id )
+    }
 }
