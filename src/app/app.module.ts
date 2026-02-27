923badc3ec42f933987e82dc0a42d3a2ea7d50b6
Dhanvi1 <danydhanvith@gmail.com>
2026-02-27 14:39:54 +0530

    creating everything modular based

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { App } from './app.js'
 import { HeaderComponent } from './header/header.component.js';
 import { UserComponent } from './user/user.component.js';
-import { TasksComponent } from './tasks/tasks.component.js';
+import { SharedModule } from './shared/shared.module.js';
+import { TasksModule } from './tasks/tasks.module.js';
+
 
 @NgModule({
-    declarations: [App],
+    declarations: [App, HeaderComponent, UserComponent],
     bootstrap: [App],
-    imports : [BrowserModule, HeaderComponent, UserComponent, TasksComponent]
+    imports : [BrowserModule, SharedModule, TasksModule]  // All the builtin pipes are imported here
 })
 
 export class AppModule {
