7d8093a0eacce52093b6bff9dcd402d730b2a06b
Dhanvi1 <danydhanvith@gmail.com>
2026-02-27 14:36:42 +0530

    creating shared module for card component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { Card } from "./card/card.component";
+
+@NgModule({
+    declarations: [Card],
+    exports: [Card]
+})
+export class SharedModule {
+
+}
